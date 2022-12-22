df6d0a820c59ac7fa5e2a3fb818cbe02ba14ce13
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-12-22 17:08:00 +0000

    updated comments

--- searching-algorithms/linear-search/visual-basic/linear_search_for.vb
+++ searching-algorithms/linear-search/visual-basic/linear_search_for.vb
@@ -1,15 +1,12 @@
-/*
-Raspberry Pi Foundation
-Usage licensed under CC BY-SA 4
-Note: This file is designed to be copied out and compiled on your machine.
-To run this file you need to:
-1. Copy the contents
-2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
-3. Compile the program
-4. Run the program
-*/
-    
-Imports System
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
 
 Module LinearSearch
 
