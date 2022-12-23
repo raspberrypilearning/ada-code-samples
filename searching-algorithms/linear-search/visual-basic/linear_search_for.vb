01d4f18599ded6af58fe92a541da85a181fcd3d8
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-12-23 09:38:15 +0000

    changed comments

--- searching-algorithms/linear-search/visual-basic/linear_search_for.vb
+++ searching-algorithms/linear-search/visual-basic/linear_search_for.vb
@@ -1,6 +1,5 @@
 ' Raspberry Pi Foundation
 ' Usage licensed under CC BY-SA 4
-' Note: This file is designed to be copied out and compiled on your machine.
 ' To run this file you need to:
 ' 1. Copy the contents
 ' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
