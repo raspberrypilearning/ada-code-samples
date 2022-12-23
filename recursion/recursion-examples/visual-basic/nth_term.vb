3c0c04ba1aca5884ddff1f955049c9b9f808a4ad
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-12-23 09:39:25 +0000

    update comments

--- recursion/recursion-examples/visual-basic/nth_term.vb
+++ recursion/recursion-examples/visual-basic/nth_term.vb
@@ -1,9 +1,8 @@
 ' Raspberry Pi Foundation
 ' Usage licensed under CC BY-SA 4
-' Note: This file is designed to be copied out and compiled on your machine.
 ' To run this file you need to:
 ' 1. Copy the contents
-' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 2. Paste them into an IDE of your choice (Visual Studio, for example)
 ' 3. Compile the program
 ' 4. Run the program
 
