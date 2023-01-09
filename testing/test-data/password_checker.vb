aaa7903b982feafa919d4caea5f2f1f7ab4fb2f1
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-09 15:38:27 +0000

    Create password_checker.vb

---
+++ testing/test-data/password_checker.vb
@@ -0,0 +1,33 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB programModule Program
+
+    Sub Main()
+        Dim passwordToCheck As String = "test"
+        Dim result As Boolean = CheckPassword(passwordToCheck)
+        Console.WriteLine(result)
+
+    End Sub
+
+    Function CheckPassword(ByVal password As String) As Boolean
+        Dim isValid As Boolean = True
+
+        If password.Length < 8 OrElse password.Length > 14 Then
+            isValid = False
+        End If
+
+        Return isValid
+    End Function
+
+
+
+End Module
