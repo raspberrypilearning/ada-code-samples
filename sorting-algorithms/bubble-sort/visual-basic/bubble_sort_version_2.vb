66ccf6d7275d2895f72bf17fde32dcfa07aee76a
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-09 12:47:53 +0000

    corrected comments

--- sorting-algorithms/bubble-sort/visual-basic/bubble_sort_version_2.vb
+++ sorting-algorithms/bubble-sort/visual-basic/bubble_sort_version_2.vb
@@ -12,10 +12,10 @@ Module Program
     ' The Main subroutine is the default entry point for a VB program
     Sub Main()
         Dim testItems As Integer() = {43, 21, 2, 50, 3, 80, 35, 7, 64} 'Array of test data
-        'Dim testItems As Integer() = {80, 64, 50, 43, 35, 21, 7, 3, 2}; // Least sorted
-        'Dim testItems As Integer() = {2, 3, 7, 35, 43, 21, 50, 64, 80}; // Nearly sorted
-        'Dim testItems As Integer() = {2, 3, 7, 21, 35, 43, 50, 64, 80}; // Sorted
-        'Dim testItems As Integer() = {43, 21, 2, 50, 3, 80, 35, 7, 64}; // Random
+        'Dim testItems As Integer() = {80, 64, 50, 43, 35, 21, 7, 3, 2}; 'Least sorted
+        'Dim testItems As Integer() = {2, 3, 7, 35, 43, 21, 50, 64, 80}; 'Nearly sorted
+        'Dim testItems As Integer() = {2, 3, 7, 21, 35, 43, 50, 64, 80}; 'Sorted
+        'Dim testItems As Integer() = {43, 21, 2, 50, 3, 80, 35, 7, 64}; 'Random
         Console.WriteLine("### Bubble sort version 2 (while and for loops) ###")
         Console.WriteLine("[{0}]", String.Join(", ", testItems)) 'Display array of test data
         BubbleSortVersion2(testItems)
