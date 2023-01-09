68f5926f34fc2b1659f42ff2dfdd455bd16459e5
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-09 15:33:07 +0000

    Create syntax_error.vb

---
+++ testing/syntax-error/visual-basic/syntax_error.vb
@@ -0,0 +1,27 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB programModule Program
+    Sub Main()
+
+        Console.WriteLin("Please enter your name: ")
+        Dim name As String = Console.ReadLine()
+
+        If name = "Ada" Then
+            Console.WriteLine("Hi Ada, you are one of my heroes!")
+        Else
+            Console.WriteLine("Hello " & name)
+        End If
+
+    End Sub
+
+
+End Module
