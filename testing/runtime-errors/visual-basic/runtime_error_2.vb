e255de9de31251cd15546dfe95dee1d984078662
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-09 15:27:12 +0000

    Create runtime_error_2.vb

---
+++ testing/runtime-errors/visual-basic/runtime_error_2.vb
@@ -0,0 +1,22 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB programModule Program
+    Sub Main()
+
+    Dim file As String = "highscores.txt"
+    Dim contents As String() = System.IO.File.ReadAllLines(file)
+    Console.WriteLine(contents)
+
+    End Sub
+
+
+End Module
