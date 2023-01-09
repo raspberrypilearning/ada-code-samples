c306f67f60874cfb03b19f917e23448cfbbc7551
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-09 15:12:47 +0000

    Create logic_error.vb

---
+++ testing/logic-error/logic_error.vb
@@ -0,0 +1,26 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB programModule Program
+    Sub Main()
+        Dim amount As Double = 130.4
+        Dim result As Double = AddVat(amount)
+        Console.WriteLine(result)
+    End Sub
+
+    Function AddVat(ByVal amount As Double) As Double
+        Dim RATE As Integer = 20
+        Dim vat As Double = amount * RATE
+        Dim withVat As Double = amount + vat
+        Return withVat
+    End Function
+
+End Module
