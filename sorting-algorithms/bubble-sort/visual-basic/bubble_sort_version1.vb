e306bbd17af2d11a01baee8dbecfc10564a651cc
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-09 12:08:51 +0000

    Update bubble_sort_version1.vb

--- sorting-algorithms/bubble-sort/visual-basic/bubble_sort_version1.vb
+++ sorting-algorithms/bubble-sort/visual-basic/bubble_sort_version1.vb
@@ -1,11 +1,23 @@
-Public Shared Sub Main()
-        Dim testItems As Integer() = New Integer() {43, 21, 2, 50, 3, 80, 35, 7, 64}
-        Console.WriteLine("### Bubble sort version 1 (for loops) ###")
-        Console.WriteLine("[{0}]", String.Join(", ", testItems))
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim testItems As Integer() = {43, 21, 2, 50, 3, 80, 35, 7, 64} 
+        Console.WriteLine("### Bubble sort version 1 (for loop) ###")
+        Console.WriteLine("[{0}]", String.Join(", ", testItems))  ' Display array of test data
         BubbleSortVersion1(testItems)
     End Sub
 
-    Public Shared Sub BubbleSortVersion1(ByVal items As Integer())
+    Sub BubbleSortVersion1(ByVal items As Integer())
         Dim numItems As Integer = items.Length
 
         For passNum As Integer = 1 To numItems - 1
@@ -22,3 +34,5 @@ Public Shared Sub Main()
             Console.WriteLine("[{0}]", String.Join(", ", items))
         Next
     End Sub
+
+End Module
