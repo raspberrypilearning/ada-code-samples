35df3f5223622710985f12f456821b906a57e12e
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-12 16:46:28 +0000

    Create read_text_file_all.vb

---
+++ file-handling/handling-text-files/visual-basic/read_text_file_all.vb
@@ -0,0 +1,27 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program (make sure the text file is in the same folder)
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        ReadAll()
+    End Sub
+
+    Sub ReadAll()
+        Dim rhyme As String
+
+        Using reader As System.IO.StreamReader = New System.IO.StreamReader("bridge.txt")
+            rhyme = reader.ReadToEnd()
+        End Using
+
+        Console.WriteLine(rhyme)
+    End Sub
+
+End Module
