d55a0a02a05e72d4795fdc75069aad20c59d9a4b
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-12 16:58:14 +0000

    Create read_text_file_exception.vb

---
+++ file-handling/handling-text-files/visual-basic/read_text_file_exception.vb
@@ -0,0 +1,34 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        ReadFileWithExceptionHandler()
+    End Sub
+
+    Sub ReadFileWithExceptionHandler()
+        Dim fileName As String = "twynkle.txt"
+        Try
+            Using reader As System.IO.StreamReader = New System.IO.StreamReader(fileName)
+                Dim line As String
+                line = reader.ReadLine()
+                Do Until line Is Nothing
+                    Console.WriteLine(line)
+                    line = reader.ReadLine()
+                Loop
+            End Using
+        Catch ex As Exception
+            Console.WriteLine($"{fileName} could not be found")
+        End Try
+    End Sub
+
+
+End Module
