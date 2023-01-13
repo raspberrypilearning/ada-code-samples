fec2e2ad39ca22ad12fa7b5c87e78fd449828cb0
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-13 15:23:25 +0000

    Create count_controlled_loop.vb

---
+++ gcse-programming-concepts/programming-constructs/visual-basic/count_controlled_loop.vb
@@ -0,0 +1,20 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        For i As Integer = 0 To 2
+            Console.WriteLine("Hip, Hip, Hooray!")
+        Next
+    End Sub
+
+
+End Module
