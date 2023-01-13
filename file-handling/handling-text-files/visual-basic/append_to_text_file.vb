e85331a4d44e0728d47914806cdba3e84f9604ca
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-13 13:46:25 +0000

    Create append_to_text_file.vb

---
+++ file-handling/handling-text-files/visual-basic/append_to_text_file.vb
@@ -0,0 +1,28 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        AppendToFile()
+    End Sub
+
+    Sub AppendToFile()
+        Using writer As System.IO.StreamWriter = New System.IO.StreamWriter("twinkle.txt", True)
+            writer.WriteLine("")
+            writer.WriteLine("When the blazing sun is gone")
+            writer.WriteLine("When he nothing shines upon,")
+            writer.WriteLine("Then you show your little light,")
+            writer.WriteLine("Twinkle, twinkle, all the night.")
+        End Using
+    End Sub
+
+
+End Module
