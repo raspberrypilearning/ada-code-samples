13fdb7f87b54bc1b1967f8a72807336336f52c7f
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-13 15:16:18 +0000

    Create string_to_float.vb

---
+++ gcse-programming-concepts/casting/visual-basic/string_to_float.vb
@@ -0,0 +1,26 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        DoubleIt()
+    End Sub
+
+    Sub DoubleIt()
+        Console.WriteLine("Enter a number ")
+        Dim number As String = Console.ReadLine()
+        Dim floatNumber As Single = Single.Parse(number)
+        Dim answer As Single = floatNumber * 2.0F
+        Console.WriteLine(answer)
+    End Sub
+
+
+End Module
