26ae4f17b8851e4c067b0681a791d2a1734a1d1e
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-13 13:53:36 +0000

    Create write_to_text_file.vb

---
+++ file-handling/handling-text-files/visual-basic/write_to_text_file.vb
@@ -0,0 +1,27 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        WriteToFile()
+    End Sub
+
+    Sub WriteToFile()
+        Using writer As System.IO.StreamWriter = New System.IO.StreamWriter("twinkle.txt", False)
+            writer.WriteLine("Twinkle, twinkle, little star,")
+            writer.WriteLine("How I wonder what you are!")
+            writer.WriteLine("Up above the world so high,")
+            writer.WriteLine("Like a diamond in the sky.")
+        End Using
+    End Sub
+
+
+End Module
