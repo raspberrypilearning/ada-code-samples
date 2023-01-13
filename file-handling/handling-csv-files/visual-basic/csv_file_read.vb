465646e990fb482ba8255ecb337b323fb0e75093
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-13 14:10:00 +0000

    Create csv_file_read.vb

---
+++ file-handling/handling-csv-files/visual-basic/csv_file_read.vb
@@ -0,0 +1,34 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        ReadCsv()
+    End Sub
+
+    Sub ReadCsv()
+        Dim fileName As String = "movies.csv"
+        Using reader As System.IO.StreamReader = New IO.StreamReader(fileName)
+            Dim record As String
+            record = reader.ReadLine()
+
+            While record IsNot Nothing
+                Dim movieData As String() = record.Split(",")
+                Dim title As String = movieData(0)
+                Dim rating As String = movieData(4)
+                Console.WriteLine($"Movie name: {title}, Rating: {rating}")
+                record = reader.ReadLine()
+            End While
+        End Using
+    End Sub
+
+
+End Module
