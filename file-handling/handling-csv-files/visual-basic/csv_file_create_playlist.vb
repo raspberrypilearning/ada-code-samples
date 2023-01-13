537f4960539d98c9113d4588ce07169a2b86ff3c
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-13 14:15:23 +0000

    Create csv_file_create_playlist.vb

---
+++ file-handling/handling-csv-files/visual-basic/csv_file_create_playlist.vb
@@ -0,0 +1,27 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        CreatePlaylist()
+    End Sub
+
+    Sub CreatePlaylist()
+        Dim fileName As String = "playlist.csv"
+        Dim header As String = "Title,Artist,Duration"
+
+        Using writer As System.IO.StreamWriter = New System.IO.StreamWriter(fileName, False)
+            writer.WriteLine(header)
+        End Using
+    End Sub
+
+
+End Module
