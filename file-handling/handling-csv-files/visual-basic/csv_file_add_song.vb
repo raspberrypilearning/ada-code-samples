d912828cd3a39d7682b1c475b500a486fb5732ba
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-13 14:18:48 +0000

    Create csv_file_add_song.vb

---
+++ file-handling/handling-csv-files/visual-basic/csv_file_add_song.vb
@@ -0,0 +1,28 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        AddSong("Happy",
+                "Pharrell Williams",
+                "03:55")
+    End Sub
+
+    Sub AddSong(ByVal title As String, ByVal artist As String, ByVal duration As String)
+        Dim newSong As String = title & "," & artist & "," & duration
+
+        Using writer As System.IO.StreamWriter = New System.IO.StreamWriter("playlist.csv", True)
+            writer.WriteLine(newSong)
+        End Using
+    End Sub
+
+
+End Module
