c1edf91a7cbbce05587c4d9173dd5805c8687a6c
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 15:11:31 +0000

    Create random_number.vb

---
+++ programming-concepts/random-numbers/visual-basic/random_number.vb
@@ -0,0 +1,19 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim rnd As Random = New Random()
+        Dim random_number As Integer = rnd.[Next](1, 100)
+        Console.WriteLine(random_number)
+    End Sub
+
+End Module
