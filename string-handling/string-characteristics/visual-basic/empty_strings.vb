04aeef3d12200d41564cd96d939939a1d89023ed
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 16:54:55 +0000

    Create empty_strings.vb

---
+++ string-handling/string-characteristics/visual-basic/empty_strings.vb
@@ -0,0 +1,32 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        DemonstrateEmptyStrings()
+    End Sub
+
+    Sub DemonstrateEmptyStrings()
+        Dim emptyString As String = ""
+        Console.WriteLine(emptyString.Length)
+
+        Dim stringWithSpace As String = " "
+        Console.WriteLine(stringWithSpace.Length)
+
+        If emptyString = stringWithSpace Then
+            Console.WriteLine("We are the same")
+        Else
+            Console.WriteLine("We are different")
+        End If
+    End Sub
+
+
+End Module
