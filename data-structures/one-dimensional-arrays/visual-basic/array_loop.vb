c23248122a7765156e6706a8acf5dffe33bcfa1c
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 12:14:51 +0000

    Create array_loop.vb

---
+++ data-structures/one-dimensional-arrays/visual-basic/array_loop.vb
@@ -0,0 +1,26 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim emotions(5) As String  'Declare empty array with 6 elements
+        emotions(0) = "amazed"
+        emotions(1) = "delighted"
+        emotions(2) = "ecstatic"
+        emotions(3) = "enthusiastic"
+        emotions(4) = "lively"
+        emotions(5) = "happy"
+
+        For i As Integer = 0 To 5
+            Console.WriteLine(emotions(i))
+        Next
+    End Sub
+End Module
