1c99afe7dffba5b01ba1c6ed322112bfb717a8dc
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 17:05:29 +0000

    Create change_character.vb

---
+++ string-handling/string-mutability/visual-basic/change_character.vb
@@ -0,0 +1,25 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        ChangeCharacter()
+    End Sub
+
+    Sub ChangeCharacter()
+        Dim welcome As String = "Hello Wirld"
+        Dim temp As String = welcome.Substring(0, 7) & "o"c & welcome.Substring(8, 3)
+        welcome = temp
+        Console.WriteLine(welcome)
+    End Sub
+
+
+End Module
