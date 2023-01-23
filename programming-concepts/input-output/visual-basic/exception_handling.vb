64e40fc0243655de2261f2559b28620812f87558
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 13:55:10 +0000

    Create exception_handling.vb

---
+++ programming-concepts/input-output/visual-basic/exception_handling.vb
@@ -0,0 +1,23 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Console.WriteLine("Enter a number")
+        Try
+            Dim userInput As String = Console.ReadLine()
+            Dim numValue As Integer = Int32.Parse(userInput)
+        Catch ex As FormatException
+            Console.WriteLine("You must enter a numeric value")
+        End Try
+    End Sub
+
+End Module
