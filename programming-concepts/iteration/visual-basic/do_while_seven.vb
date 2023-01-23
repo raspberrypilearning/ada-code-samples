1278f7b1adf7c80e0df012d688adf0aa90319a9e
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 14:26:07 +0000

    Create do_while_seven.vb

---
+++ programming-concepts/iteration/visual-basic/do_while_seven.vb
@@ -0,0 +1,22 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim count As Integer = 7
+
+        Do
+            Console.WriteLine(count)
+            count = count + 1
+        Loop While count <= 5
+    End Sub
+
+End Module
