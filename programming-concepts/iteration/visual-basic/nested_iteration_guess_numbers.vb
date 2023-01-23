f4157c49646b39a59473aaa2f414810e97cb9973
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 14:48:43 +0000

    Create nested_iteration_guess_numbers.vb

---
+++ programming-concepts/iteration/visual-basic/nested_iteration_guess_numbers.vb
@@ -0,0 +1,32 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim numbersToGuess As Integer() = {1, 4, 8, 3, 10}
+        Console.WriteLine("Guess my numbers, each number is between 1 and 10")
+
+        For Each number As Integer In numbersToGuess
+            Console.WriteLine("Enter a number to guess: ")
+            Dim userInput As String = Console.ReadLine()
+            Dim guess As Integer = Int32.Parse(userInput)
+
+            While guess <> number
+                Console.WriteLine("Enter a number to guess: ")
+                userInput = Console.ReadLine()
+                guess = Int32.Parse(userInput)
+            End While
+
+            Console.WriteLine("Well done, next number to guess")
+        Next
+    End Sub
+
+End Module
