f45a2ffd93bfe6bb5a0d352843f859e30dea3480
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 14:19:07 +0000

    Create range_check.vb

---
+++ programming-concepts/input-output/visual-basic/range_check.vb
@@ -0,0 +1,25 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Console.WriteLine("Enter a week of the year: ")
+        Dim userInput As String = Console.ReadLine()
+        Dim week As Integer = Int32.Parse(userInput)
+
+        If week >= 1 AndAlso week <= 52 Then
+            Console.WriteLine($"You have chosen week {week}")
+        Else
+            Console.WriteLine("A week must be between 1-52")
+        End If
+    End Sub
+
+End Module
