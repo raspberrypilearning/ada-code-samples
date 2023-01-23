864c5ac5652201bf9f18b5f8f15594b64c5fbd83
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 14:34:23 +0000

    Create for_one_to_five.vb

---
+++ programming-concepts/iteration/visual-basic/for_one_to_five.vb
@@ -0,0 +1,19 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        For count As Integer = 1 To 5
+            Console.WriteLine(count)
+        Next
+    End Sub
+
+End Module
