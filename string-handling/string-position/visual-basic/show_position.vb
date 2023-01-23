0e8f38e9ebe3c85b32714e83f1fa61cb2cbf321b
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 17:23:17 +0000

    Create show_position.vb

---
+++ string-handling/string-position/visual-basic/show_position.vb
@@ -0,0 +1,21 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim subject As String = "Computer Science"
+        Dim letterSought As Char = "m"
+        Dim position As Integer = subject.IndexOf(letterSought)
+        Console.WriteLine($"{letterSought} found at: {position}")
+    End Sub
+
+
+End Module
