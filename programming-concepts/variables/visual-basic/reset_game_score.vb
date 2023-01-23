2d0e96421359de41ca8db1a63787f01abb5677db
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 16:41:13 +0000

    Create reset_game_score.vb

---
+++ programming-concepts/variables/visual-basic/reset_game_score.vb
@@ -0,0 +1,26 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim score As Integer
+        Console.WriteLine("Would you like to start a new game?")
+        Dim response As String = Console.ReadLine()
+
+        If response = "YES" Then
+            score = 0
+        End If
+
+        Console.WriteLine(score)
+    End Sub
+
+
+End Module
