f56cd9f63ccf3441832699fcfbb2716a49d6e8f8
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-23 12:17:30 +0000

    Create create_array.vb

---
+++ data-structures/one-dimensional-arrays/visual-basic/create_array.vb
@@ -0,0 +1,24 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        'Declare empty array with 6 elements
+        Dim emotions(5) As String  
+        'Put 6 items into the array
+        emotions(0) = "amazed"
+        emotions(1) = "delighted"
+        emotions(2) = "ecstatic"
+        emotions(3) = "enthusiastic"
+        emotions(4) = "lively"
+        emotions(5) = "happy"
+    End Sub
+End Module
