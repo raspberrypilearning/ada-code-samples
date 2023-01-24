9913b8033f7da4092228fe7c167594e9ee01cb54
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-24 12:10:30 +0000

    Create flow_of_control.vb

---
+++ subroutines/define-and-call/visual-basic/flow_of_control.vb
@@ -0,0 +1,21 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        Dim minutes As Integer = 90
+        Dim seconds As Integer = minutes * 60
+        Dim milliseconds As Integer = seconds * 1000
+        Console.WriteLine($"Seconds: {seconds}, Milliseconds: {milliseconds}")
+    End Sub
+
+
+End Module
