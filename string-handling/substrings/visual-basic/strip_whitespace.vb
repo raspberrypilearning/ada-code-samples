6c6d56f002531072ff5d5f5a4f36af37b673fc7d
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-24 11:34:30 +0000

    Create strip_whitespace.vb

---
+++ string-handling/substrings/visual-basic/strip_whitespace.vb
@@ -0,0 +1,24 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+Module Program
+
+    ' The Main subroutine is the default entry point for a VB program
+    Sub Main()
+        StripWhitespace()
+    End Sub
+
+    Sub StripWhitespace()
+        Dim myString As String = "     Isaac Computer Science  " & vbLf
+        Dim strippedString As String = myString.Trim()
+        Console.WriteLine(strippedString)
+    End Sub
+
+
+End Module
