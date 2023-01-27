f2e8e4cc9e998d670665aba84e82dfc1af78af0f
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-27 11:39:10 +0000

    Create boat_class.vb

---
+++ object-oriented-programming/inheritance/visual-basic/boat_class.vb
@@ -0,0 +1,44 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+
+Class Boat
+
+    Private name As String
+    Private length As Single
+    Private capacity As Integer
+    Private berths As Integer
+    Protected unitCost As Single
+
+    ' Constructor method
+    Public Sub New(ByVal givenName As String, ByVal givenLength As Single, ByVal givenCapacity As Integer, ByVal givenBerths As Integer, ByVal givenUnitCost As Single)
+        name = givenName
+        length = givenLength
+        capacity = givenCapacity
+        berths = givenBerths
+        unitCost = givenUnitCost
+    End Sub
+
+End Class
+
+
+Module Program
+
+    ' The Main method is the default entry point for all VB programs
+    Sub Main()
+        Dim name = "Sir David Attenborough"
+        Dim length = 128.9F
+        Dim capacity = 88
+        Dim berths = 90
+        Dim cost = 2.0E+8F
+        Dim boaty As Boat = New Boat(name, length, capacity, berths, cost)  ' Instantiate a new boat object
+    End Sub
+
+
+End Module
