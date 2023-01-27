f8e1afff78ec70bf217a601fc55cd2eed58954f7
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-01-27 15:20:21 +0000

    Create calculate_area.vb

---
+++ subroutines/procedure-function/visual-basic/calculate_area.vb
@@ -0,0 +1,28 @@
+' Raspberry Pi Foundation
+' Usage licensed under CC BY-SA 4
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+
+Module Program
+
+    ' The Main method is the default entry point for all VB programs
+    Sub Main()
+        Dim length As Single = 12
+        Dim result = CalculateArea(length)
+        Console.WriteLine(result)
+    End Sub
+
+
+    ' Calculates the area of a square with sides of a given length
+    Function CalculateArea(ByVal length As Single) As Single
+        Dim area = length * length
+        Return area
+    End Function
+
+
+End Module
