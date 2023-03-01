09bf5a0f4e064b9c73127666cf18709235079c6c
Alex Parry <alex.parry@raspberrypi.org>
2023-03-01 12:48:48 +0000

    Removed old programming concepts files

--- gcse-programming-concepts/programming-constructs/python/count_controlled_loop.py
+++
@@ -1,8 +0,0 @@
-def loop_three_times():
-    """Example of count controlled loop"""
-    for i in range(3):
-        print("Hip hip hooray!")
-
-if __name__ == '__main__':
-    loop_three_times()
-        
