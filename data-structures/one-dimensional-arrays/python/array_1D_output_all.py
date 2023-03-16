62677d3a9eb6c4cc81b99ed2033014df491527d5
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-03-16 13:08:55 +0000

    Update array_1D_output_all.py

--- data-structures/one-dimensional-arrays/python/array_1D_output_all.py
+++ data-structures/one-dimensional-arrays/python/array_1D_output_all.py
@@ -4,26 +4,23 @@
 # Usage licensed under CC BY-NC-SA 4.0
 
 
-def add_words(spelling_words):
-    """Add the words to the list""" 
-    spelling_words=["path", "floor", "sugar", "because", "beautiful", "clothes", "whole", "behind", "move", "busy"]
-
-
 def output_each_word(spelling_words):
     """Output every word in the list"""
+
+    print("### Output each word in the list ###")
+    
     for word in spelling_words: 
         print(word)
 
 
 def main():
-    # Create a list    
-    spelling_words = []  # Declare an empty list
+    # Create a list of words
+    spelling_words = ["path", "floor", "sugar",
+                      "because", "beautiful", "clothes",
+                      "whole", "behind", "move", "busy"]
 
-    # Add words to the list
-    add_words(spelling_words)
 
-    # Output every word in the list
-    print("### Output each word in the list ###")
+    # Output every word in the list    
     output_each_word(spelling_words)
 
 
