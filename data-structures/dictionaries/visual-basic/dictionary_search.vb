9e9d8c1d45ea4694d7be557a627d32a6dd743749
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-06-12 12:11:11 +0100

    Create dictionary_search.vb

---
+++ data-structures/dictionaries/visual-basic/dictionary_search.vb
@@ -0,0 +1,38 @@
+' Raspberry Pi Foundation
+' Developed as part of Ada Computer Science
+' Usage licensed under CC BY-NC-SA 4.0
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+
+Module Program
+    ' The Main subroutine is the entry point for all VB programs
+    Sub Main()
+        ' Declare and initialise the astronaut dictionary
+        Dim astronaut As Dictionary(Of String, String) = New Dictionary(Of String, String)() From {
+            {"craft", "ISS"},
+            {"name", "Sultan Al Neyadi"},
+            {"days", "14"},
+            {"country", "United Arab Emirates"}
+        }
+
+        ' Ask the user for a key to find
+        Console.WriteLine("Enter a key to find:")
+        Dim inputKey As String = Console.ReadLine()
+
+        ' Check if the key exists in the dictionary
+        If astronaut.ContainsKey(inputKey) Then
+            Console.WriteLine("Key exists!")
+            Dim value = astronaut(inputKey)
+            Console.WriteLine($"Key: {inputKey}, value: {value}")
+        Else
+            Console.WriteLine("That key does not exist")
+        End If
+    End Sub
+
+
+End Module
