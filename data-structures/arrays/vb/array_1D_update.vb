de35a41bce117b3b8a11aa108824e5a874c1d657
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-06-29 16:25:32 +0100

    Create array_1D_update.vb

---
+++ data-structures/arrays/vb/array_1D_update.vb
@@ -0,0 +1,28 @@
+' Raspberry Pi Foundation
+' Developed as part of Ada Computer Science
+' Usage licensed under CC BY-NC-SA 4.0
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+
+Module Program
+
+    ' The Main subroutine is the entry point for all VB programs
+    Sub Main()
+        ' Declare and initialise the array of words
+        Dim spellingWords = New String(4) {"me", "do", "her", "it", "him"}
+
+        ' Update the word stored at index 4
+        spellingWords(4) = "log"
+
+        ' Output the updated word
+        Dim updatedWord = spellingWords(4)
+        Console.WriteLine($"The last word has been changed to {updatedWord}")
+    End Sub
+
+
+End Module
