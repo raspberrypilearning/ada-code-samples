5148fb1aef9d942145da1410b2e53fe98c9e6c18
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2023-06-29 16:12:56 +0100

    Create array_1D_access.vb

---
+++ data-structures/arrays/vb/array_1D_access.vb
@@ -0,0 +1,24 @@
+' Raspberry Pi Foundation
+' Developed as part of Ada Computer Science
+' Usage licensed under CC BY-NC-SA 4.0
+' Note: This file is designed to be copied out and compiled on your machine.
+' To run this file you need to:
+' 1. Copy the contents
+' 2. Paste them into the Visual Basic IDE of your choice (Visual Studio, for example)
+' 3. Compile the program
+' 4. Run the program
+
+
+Module Program
+
+    ' The Main subroutine is the entry point for all VB programs
+    Sub Main()
+        ' Declare and initialise the array of words
+        Dim spellingWords = New String(4) {"me", "do", "her", "it", "him"}
+
+        ' Output the first word
+        Dim word = spellingWords(0)
+        Console.WriteLine($"The first word is {word}")
+    End Sub
+
+End Module
