51d83ab8ff00c7ffea9cb8172b28fed180e37439
Michael Parker <54761507+micfun123@users.noreply.github.com>
2023-07-07 19:51:10 +0100

    Add files via upload

---
+++ compression/techniques/c/RLE.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void RLE(char* str)
+{
+    char* p = str;
+    int count = 1;
+    while (*p != '\0') {
+        if (*p == *(p+1)) {
+            count++;
+        } else {
+            printf("%d%c", count, *p);
+            count = 1;
+        }
+        p++;
+    }
+    
+}
+
+int main(void) {
+    char str[] = "aaabbcd";
+    RLE(str);
+    return 0;
+}
