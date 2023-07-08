7294dcfa1b1d295e4a74fcf5f1cf4df0bb8efd32
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-08 08:32:28 +0100

    print backwards with recursive

---
+++ recursion/recursion-examples/c/print_backwards_recursive.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_backwards(char *str) { // Recursively prints a string backwards
+    if (*str == '\0') {
+        return;
+    }
+    print_backwards(str + 1); // Recursive call
+    printf("%c", *str);
+}
+
+int main() { // Driver code
+    char str[] = "Hello World!";
+    print_backwards(str);
+    return 0;
+}
